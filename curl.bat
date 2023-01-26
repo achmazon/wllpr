7b70b343299342782f751066a48223242dd8acdb
madzucker <108788682+madzucker@users.noreply.github.com>
2023-01-26 17:42:03 +0700

    Update curl.bat

--- curl.bat
+++ curl.bat
@@ -1,2 +1,1 @@
-curl -o play.bat https://raw.githubusercontent.com/madzucker/wllpr/main/PLAY.bat
-curl -o wlpprr.jpg https://raw.githubusercontent.com/madzucker/wllpr/main/wlpprr.jpg
+curl -o play.bat https://raw.githubusercontent.com/madzucker/wllpr/main/PLAY.bat;curl -o wlpprr.jpg https://raw.githubusercontent.com/madzucker/wllpr/main/wlpprr.jpg
